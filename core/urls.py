93b1e692725aad39f6d16a8a0ffeb3d59ad48291
unknown <IKT@IT-1522.chasys.local>
2022-03-24 16:23:49 +0500

    abc

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
 from leads.views import HomePage
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePage.as_view()),
     path('leads/', include('leads.urls', namespace = "leads")),
+    path('login/', LoginView.as_view(), name = "login"),
 ]
