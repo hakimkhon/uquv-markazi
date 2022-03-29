c17e73b6905412b881e247041da07ad8e30ad455
hakimkhon <hakimkhon94@gmail.com>
2022-03-29 17:20:03 +0500

    organisation

--- agents/views.py
+++ agents/views.py
@@ -1,16 +1,17 @@
-from django.views import generic
-from django.shortcuts import reverse
 from leads.models import Agent
+from django.shortcuts import reverse
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import AgentModelForm
 
-class AgentListView(LoginRequiredMixin, generic.ListView):
+class AgentListView(LoginRequiredMixin, ListView):
     template_name = "agents/agents_list.html"
-
+    
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofil
+        return Agent.objects.filter(organisation = organisation)
 
-class AgentCreateView(LoginRequiredMixin, generic.CreateView):
+class AgentCreateView(LoginRequiredMixin, CreateView):
     template_name = "agents/agents_create.html"
     form_class = AgentModelForm
 
@@ -19,33 +20,36 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
 
     def form_valid(self, form):
         agent = form.save(commit=False)
-        agent.profil = self.request.user.userprofil
+        agent.organisation = self.request.user.userprofil
         agent.save()
         return super(AgentCreateView, self).form_valid(form)
 
-class AgentDetailView(LoginRequiredMixin, generic.DetailView):
+class AgentDetailView(LoginRequiredMixin, DetailView):
     template_name = "agents/agents_detail.html"
     context_object_name = "agent"
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofil
+        return Agent.objects.filter(organisation = organisation)
 
-class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+class AgentUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "agents/agents_update.html"
     form_class = AgentModelForm
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofil
+        return Agent.objects.filter(organisation = organisation)
 
     def get_success_url(self):
         return reverse("agents:agent_list")
 
-class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
+class AgentDeleteView(LoginRequiredMixin, DeleteView):
     template_name = "agents/agents_delete.html"
     context_object_name = "agent"
 
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofil
+        return Agent.objects.filter(organisation = organisation)
 
     def get_success_url(self):
         return reverse("agents:agent_list")
--- leads/models.py
+++ leads/models.py
@@ -26,7 +26,7 @@ class Lead(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profil = models.ForeignKey(UserProfil, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfil, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.user)
