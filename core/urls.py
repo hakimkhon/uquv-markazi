39484394431e265c95949732d7f0570d3b590808
hakimkhon <hakimkhon94@gmail.com>
2022-03-31 11:02:10 +0500

    galeriya

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from leads.views import HomePage, SignupView
+from leads.views import Galereya, HomePage, SignupView
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name = "login"),
     path('signup/', SignupView.as_view(), name = "signup"),
     path('logout/', LogoutView.as_view(), name = "logout"),
+    path('galery/', Galereya.as_view(), name="galery"),
 ]
